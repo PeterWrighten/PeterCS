e373030db0e956a507caee37670f8b8e9cc180c9
PeterWrighten <peterwrighten@gmail.com>
2021-10-22 17:21:17 +0900

    update

---
+++ DataStructure/Stanford_CS106B/Lecture1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "console.h"
+using namespace std;
+
+// The C++ compiler will look for a function
+// called "main"
+int main() {
+   cout << "Hello, world!" << endl;
+   return 0;  // must return an int to indicate
+              // successful program completion
+}
