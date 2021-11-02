618292f3165cd53da3afd94cbb3d401481eb3c0d
PeterWrighten <peterwrighten@gmail.com>
2021-11-02 15:35:28 +0900

    demo multi .java file

--- DataStructure/Berkley_CS61B/assn1/Dog_multi/Dog.java
+++ DataStructure/Berkley_CS61B/assn1/Dog_multi/Dog.java
@@ -1,5 +1,17 @@
 public class Dog{
-	public static void makeNoise(){
-		System.out.println("Bark!");
+	public int weight;
+	public Dog(int w){
+		weight = w;
+	}
+	public void makeNoise(){
+		if(weight < 10){
+			System.out.println("Yip!");
+		}else if(weight < 30){
+			System.out.println("Wooooh!");
+		}else{
+			System.out.println("Bark!");
+
+		}
+
 	}
 }
--- DataStructure/Berkley_CS61B/assn1/Dog_multi/DogLauncher.java
+++ DataStructure/Berkley_CS61B/assn1/Dog_multi/DogLauncher.java
@@ -1,6 +1,8 @@
 public class DogLauncher{
 	public static void main(String[] args){
-		Dog.makeNoise();
+
+		Dog d = new Dog(2);
+		d.makeNoise();
 	}
 
 }
