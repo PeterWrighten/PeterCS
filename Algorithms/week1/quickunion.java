c9a44b9c417ca928dee2ddc02bb1d0d07fd7c5a2
PeterWrighten <peterwrighten@gmail.com>
2021-11-03 17:22:46 +0900

    Update quickunion.java

--- Algorithms/week1/quickunion.java
+++ Algorithms/week1/quickunion.java
@@ -1,6 +1,6 @@
-public class Quickunion{
+public class quickunion{
 	private int[] id;
-	public Quickunion(int N){
+	public quickunion(int N){
 		id = new int[N];
 		for(int i = 0; i < id.length; ++i){
 			id[i] = i;
@@ -8,9 +8,38 @@ public class Quickunion{
 
 	}
 
-	public boolean find(int p, int q){
-		
+	public int root(int p){
+		while(id[p] != p){
+			p = id[p];
+		}
+		return p;
+	}
 
+	public boolean find(int p, int q){
+		return root(p) == root(q);
+
+	}
+
+	public void union(int p, int q){
+		if(root(p) == root(q)){
+			System.out.println("Connected! Change another node! ");
+		}else{
+			id[root(p)] = root(q);
+			System.out.println("Dynamic Connectivity: [");
+			for(int x = 0; x < id.length; ++x){
+				System.out.println(" ("+ x + "," + id[x] + ") ");
+			}
+			System.out.println(" ]");
+		}
+	}
+
+	public static void main(String[] args){
+		quickunion d = new quickunion(9);
+		d.union(0, 2);
+		d.union(1, 7);
+		d.union(8, 2);
+		d.union(3, 5);
+		d.union(5, 3);
 	}
 
 
