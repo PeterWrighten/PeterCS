acd4833d87e612774bd5c1806ebf36ea36f4e266
PeterWrighten <peterwrighten@gmail.com>
2021-11-12 20:16:11 +0900

    update: fixbug_Stack.cc; use Template

---
+++ DataStructure/DataStructure_CPP/2.Stack and Queue/Stack_T.cc
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+
+class Stack{
+
+private:
+    struct Node{
+    public:
+        T item;
+        Node *next;
+    };
+    Node *top = nullptr;
+    T i;
+public:
+    void push(T i);
+    void pop();    
+};
+template<class T>
+void Stack<T>::push(T i){
+    Node *oldtop = top;
+        top = new Node();
+        top->item = i;
+        top->next = oldtop;
+}
+
+template<class T>
+void Stack<T>::pop(){
+    Node *p = top;
+    top = top->next;
+    delete p;
+    
+}
+int main(){
+    Stack<int> *s = new Stack<int>();
+
+}
+
+
+
