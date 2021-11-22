80016c575688dd794f5e7d33dfba0efb4388f179
PeterWright <peterwrighten@gmail.com>
2021-11-22 21:55:55 +0900

    update: mergesort.java

--- Algorithms/w3_sort/mergesort_topdown.java
+++ Algorithms/w3_sort/mergesort_topdown.java
@@ -2,27 +2,35 @@ public class mergesort_topdown
 {
     private int[] a;
     private int size;
+    private int[] result;
     public mergesort_topdown(int[] b)
     {
         a = b;
         size = b.length;
+        result = new int[size];
     }
 
-    public void merge(int i, int j)
+    public void merge(int start, int mid, int end)
     {
+        for(int i = 0; i < mid + 1; i++)
+        {
+            result[i] =  a[i] < a[mid + 1 + i] ? a[i] : a[mid + 1 + i];
+        }
+        
+
 
     }
 
-    public void devide(int start, int end)
+    public void merge_sort(int start, int end)
     {
         if(start >= end)
             return;
-        
+         
         int len = end - start;
         int mid = start + len>>1;
-        
-
-
+        merge_sort(start, mid);
+        merge_sort(mid + 1, end);
+        merge(start, mid, end);
 
     }
 
