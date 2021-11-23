c0734cf25d3461ff03fbe28782d41928331467fc
PeterWright <peterwrighten@gmail.com>
2021-11-24 01:18:56 +0900

    update: rust debugger

--- Prerequisites/peter_rust/src/main.rs
+++ Prerequisites/peter_rust/src/main.rs
@@ -1,6 +1,12 @@
 #![allow(non_snake_case)]
 
+fn add(x: i32, y: i32) -> (i32)
+{
+    return x + y;
+}
 
-fn main() {
-    println!("Hello, world!");
+fn main() 
+{
+    let x = add(4, 7);
+    println!("Hello, {}", x);
 }
