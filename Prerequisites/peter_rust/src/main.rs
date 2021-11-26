b087af5fed460e902ccee43a9914d0f1145d2f0c
PeterWright <peterwrighten@gmail.com>
2021-11-27 02:22:04 +0900

    update: rust

--- Prerequisites/peter_rust/src/main.rs
+++ Prerequisites/peter_rust/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-fn add(x: i32, y: i32) -> (i32)
+fn add(x: i32, y: i32) -> i32
 {
     return x + y;
 }
@@ -9,4 +9,14 @@ fn main()
 {
     let x = add(4, 7);
     println!("Hello, {}", x);
+    let y = loop{
+        let mut z = 0;
+        if z == x  
+        {
+            break "Found x!";
+        }
+        z += 1;
+
+    };
+    println!("Found x: {}", y);
 }
