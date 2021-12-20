06776b3bfb7f2de5a3eadcad433a68fa0d54aa92
PeterWright <peterwrighten@gmail.com>
2021-12-21 03:11:49 +0900

    update: encapsulate

--- Algorithms/w5_graph_search/Depth_first_search.java
+++ Algorithms/w5_graph_search/Depth_first_search.java
@@ -1,3 +1,9 @@
 public class Depth_first_search {
     
+    public static void main(String[] args) {
+        Graph graph = new Graph(6);
+        int i = graph.V();
+        System.out.println(i);
+    }
+    
 }
